d16478c881ef6cb588884b1c280c44e37453934d
VinceJuliano <v.juliano6363@gmail.com>
2026-01-14 10:55:39 -0500

    fix: cu port

--- aocontrol/server/repusher/pushIssues.ts
+++ aocontrol/server/repusher/pushIssues.ts
@@ -179,7 +179,7 @@ query GetMessageByTags {
 
 const CUSTOM_CU_MAP: any = {
   "qNvAoz0TgcH7DMg8BCVn8jF32QH5L6T29VjHxhHqqGE": "cu6201.ao-testnet.xyz",
-  "0syT13r0s0tgPmIed95bJnuSqaD29HQNN8D3ElLSrsc": `cu6002.ao-testnet.xyz:${process.env.PORT_1}`
+  "0syT13r0s0tgPmIed95bJnuSqaD29HQNN8D3ElLSrsc": `cu6002.ao-testnet.xyz`
 }
 
 export async function missingNonceReport(txs: string[], customCu: boolean): Promise<CheckIfPushedResult> {
